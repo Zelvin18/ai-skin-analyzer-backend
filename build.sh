aa2311aa2bce924a34ea38caca00e7a86bfd7d96
Zelvin18 <magumisekelvin2023@gmail.com>
2025-04-28 20:13:57 +0300

    fix: Update build script to handle migrations

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@
 # exit on error
 set -o errexit
 
+# Install Python dependencies
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate 
+# Run migrations
+python manage.py makemigrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --no-input 
