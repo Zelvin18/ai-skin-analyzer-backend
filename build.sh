2406dd8d989f6c3ba57f6936f3cd2669bf93adb9
Zelvin18 <magumisekelvin2023@gmail.com>
2025-04-28 21:21:19 +0300

    fix: Update build script to handle username and email separately

--- build.sh
+++ build.sh
@@ -14,7 +14,11 @@ python manage.py shell << END
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(email='admin@example.com').exists():
-    User.objects.create_superuser('admin@example.com', 'admin', 'admin')
+    User.objects.create_superuser(
+        username='admin',
+        email='admin@example.com',
+        password='admin'
+    )
 END
 
 # Collect static files
